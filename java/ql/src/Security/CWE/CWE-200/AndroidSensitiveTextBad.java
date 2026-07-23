3a0ceb1bad6a180b922af7985875fb4688549f33
Donnchadh Mac Suibhne <dmacsuibhne@waratek.com>
2026-07-23 15:09:11 +0100

    IAST-96 Delete android classes

--- java/ql/src/Security/CWE/CWE-200/AndroidSensitiveTextBad.java
+++
@@ -1,9 +0,0 @@
-import android.app.Activity;
-import android.widget.TextView;
-
-public class AndroidSensitiveTextBad extends Activity {
-    void showPassword(String password) {
-TextView pwView = findViewById(R.id.pw_text);
-pwView.setText("Your password is: " + password); // BAD: password is shown immediately
-    }
-}
