3e322ae036ff369335169f5e7f48cbd59d879aee
Donnchadh Mac Suibhne <dmacsuibhne@waratek.com>
2026-07-28 17:02:41 +0100

    IAST-96 Restore comments

--- vulnerable-app/src/main/java/com/example/vulnapp/servlets/CWE_295_InsecureTrustManager.java
+++ vulnerable-app/src/main/java/com/example/vulnapp/servlets/CWE_295_InsecureTrustManager.java
@@ -34,22 +34,29 @@ public class CWE_295_InsecureTrustManager extends HttpServlet {
             context.init(null, trustManager, null);
 
             try {
-                // GOOD: instead of a custom TrustManager, add the self-signed certificate we want to
-                // trust to a key store; the resulting trustManagers will ONLY trust that certificate.
                 SSLContext goodContext = SSLContext.getInstance("TLS");
                 File certificateFile = new File("path/to/self-signed-certificate");
+                // Create a `KeyStore` with default type
                 KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+                // `keyStore` is initially empty
                 keyStore.load(null, null);
                 X509Certificate generatedCertificate;
                 try (InputStream cert = new FileInputStream(certificateFile)) {
                     generatedCertificate = (X509Certificate) CertificateFactory.getInstance("X509")
                             .generateCertificate(cert);
                 }
+                // Add the self-signed certificate to the key store
                 keyStore.setCertificateEntry(certificateFile.getName(), generatedCertificate);
+                // Get default `TrustManagerFactory`
                 TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+                // Use it with our key store that trusts our self-signed certificate
                 tmf.init(keyStore);
                 TrustManager[] trustManagers = tmf.getTrustManagers();
                 goodContext.init(null, trustManagers, null);
+                // GOOD, we are not using a custom `TrustManager` but instead have
+                // added the self-signed certificate we want to trust to the key
+                // store. Note, the `trustManagers` will **only** trust this one
+                // certificate.
 
                 URL url = new URL("https://self-signed.badssl.com/");
                 HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
